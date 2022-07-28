d97a5564d43d0c460f04a588ed188611c03b087e
seungyeonji <cocoj95@naver.com>
2022-07-28 18:19:31 +0900

    modify 2 files

--- src/Practice2.java
+++ src/Practice2.java
@@ -7,23 +7,27 @@ public class Practice2 {
 		//대문자 + 32 = 소문자, 홀수줄 대문자, 짝수줄 소문자
 		
 		for(int i=65; i<=90; i++) {
+			if(line%2==0) System.out.printf("%c\t", i+32);		//짝수면 소문자로
+			else System.out.printf("%c\t", i);						//홀수면 대문자로
+		
+//		for(int i=65; i<=90; i++) {
+//			
+//			if(line%2==0) {
+//				System.out.print((char)(i+32) + "\t");
+//			}else {
+//				char ch = (char)i;
+//				System.out.print(ch + "\t");
+//			}
 			
-			if(line%2==0) {
-				System.out.print((char)(i+32) + "\t");
-			}else {
-				char ch = (char)i;
-				System.out.print(ch + "\t");
-//			System.out.print(i+ "\t");
-
-			}
 			count++;
 			
 			if(count%5==0) {
 				System.out.println();
 				line++;
 
-				
+		}
 			
 		}
-		}}}
+	}
+}
 
--- src/RandomDemo.java
+++ src/RandomDemo.java
@@ -1,13 +1,9 @@
 
 public class RandomDemo {
 	public static void main(String[] args) {
-//		System.out.println(Math.random());
-//		int rand = (int)(Math.random() * 6 + 1);
-//		System.out.println(rand);
-		
-		
-		
-		
+		System.out.println(Math.random());
+		int rand = (int)(Math.random() * 6 + 1);
+		System.out.println(rand);
 
 	}
 }
